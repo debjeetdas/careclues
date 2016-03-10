18ad6f434a557837a86b2c70ec806aad64d4c68b
debjeetdas <debjeetster@gmail.com>
2016-03-10 16:32:35 +0530

    marketing page V1 update
    marketing page V1 update

--- marketing/_site/script/script.js
+++ marketing/_site/script/script.js
@@ -34,4 +34,40 @@ $(document).scroll(function(){
 			$('.navbar').removeClass('navbar-top-fixed-banner');
 		}
 	}
+	if($(this).scrollTop()>=($('.how-it-works-row').position().top-250)){      
+		$('#marketing-feature-image-6').css({opacity: 0.0, visibility: "visible"}).stop(true,true).animate({opacity: 1.0},1000);
+		$('#marketing-feature-image-6').siblings('.marketing-fa-arrow-right').delay(1000).fadeIn(500);
+		$('#marketing-feature-image-mobile-unselect').css({opacity: 0.0, visibility: "visible"}).stop(true,true).delay(2000).animate({opacity: 1.0},1000);
+		if (screen.width > 350){
+			$('.marketing-feature-image-mobile-hand').fadeIn().stop(true,true).delay(3000).animate({right: '-19', bottom: '71'}, 1000, function(){
+				$('.marketing-feature-image-mobile-hand').delay(100).queue(function (next) { 
+					$(this).css('width', '39px'); 
+					next(); 
+				});
+				$('.marketing-feature-image-mobile-hand').delay(200).queue(function (next) { 
+					$(this).css('width', '40px'); 
+					next(); 
+				});
+				$('#marketing-feature-image-mobile-unselect.marketing-feature-imagecommon').attr('id','').addClass('marketing-feature-image-mobile-select');
+				$('.marketing-fa-arrow-right-unselect').delay(1000).fadeIn(500);
+				$('#marketing-feature-image-8').css({opacity: 0.0, visibility: "visible"}).stop(true,true).delay(2000).animate({opacity: 1.0},2000);
+			});
+		}
+		else if (screen.width <= 350) {
+			$('.marketing-feature-image-mobile-hand').fadeIn().stop(true,true).delay(3000).animate({right: '-13',bottom: '64'}, 4000,function(){
+				$('.marketing-feature-image-mobile-hand').delay(100).queue(function (next) { 
+					$(this).css('width', '29px'); 
+					next(); 
+				});
+				$('.marketing-feature-image-mobile-hand').delay(100).queue(function (next) { 
+				  $(this).css('width', '30px'); 
+				  next(); 
+				});
+				$('#marketing-feature-image-mobile-unselect.marketing-feature-imagecommon').attr('id','').addClass('marketing-feature-image-mobile-select');
+				$('.marketing-fa-arrow-right-unselect').delay(1000).fadeIn(500);
+				$('#marketing-feature-image-8').css({opacity: 0.0, visibility: "visible"}).stop(true,true).delay(2000).animate({opacity: 1.0},2000);
+			});  
+		} 
+		$(document).off('scroll');
+    }
 });
