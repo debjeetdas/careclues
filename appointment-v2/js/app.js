066fcfd1b012604e65e1dc045591e9f04cfc75ef
Anushyab <anushyabilur@gmail.com>
2016-04-02 15:18:35 +0530

    changes in appointment v2 appointment.html

--- appointment-v2/js/app.js
+++ appointment-v2/js/app.js
@@ -1,12 +1,1 @@
-var app = angular.module("TestApp", []);
-
-app.controller("TestCtrl", ["$http", "$scope", function($http, $scope) {
-    $http.get("js/jsondata.json")
-    .success(function (response) {
-        $scope.patients = response.data;
-        // console.log("RESPONSE", response);
-    })
-    .error(function () {
-        alert("fail");
-    });
-}]);
+var app = angular.module('GalleryApp', []);
